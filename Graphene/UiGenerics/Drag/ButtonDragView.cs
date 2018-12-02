3d392f1d2194abb8e3ffc9e31c21c79b2bb5ff26
Yuri Cruz <yuriaicruz@gmail.com>
2018-12-02 02:31:33 -0200

    add draggable variance

---
+++ Graphene/UiGenerics/Drag/ButtonDragView.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Graphene.UiGenerics.Drag
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonDragView : Draggable
+    {
+        protected Button Button;
+        void Setup()
+        {
+            Button = GetComponent<Button>();
+            Button.onClick.AddListener(OnClick);
+        }
+
+        protected virtual void OnClick()
+        {
+            Invoke("Enable", 0.8f);
+        }
+
+        protected void Disable()
+        {
+            Button.interactable = false;
+        }
+
+        protected void Enable()
+        {
+            Button.interactable = true;
+        }
+    }
+}
