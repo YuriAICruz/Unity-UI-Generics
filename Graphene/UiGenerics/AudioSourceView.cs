adbe0254c68ace0b11e1ab3285f6d23c2d7a27bc
Yuri Cruz <yuriaicruz@gmail.com>
2018-12-10 11:02:22 -0200

    audiosourceview

---
+++ Graphene/UiGenerics/AudioSourceView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Graphene.UiGenerics
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSourceView : MonoBehaviour
+    {
+        private AudioSource _source;
+
+        private void Awake()
+        {
+            _source = GetComponent<AudioSource>();
+            SendMessage("Setup", SendMessageOptions.DontRequireReceiver);
+        }
+
+        public void Play()
+        {
+            _source.Play();
+        }
+        public void Play(AudioClip clip)
+        {
+            _source.clip = clip;
+            _source.Play();
+        }
+        
+
+        public void Stop()
+        {
+            _source.Stop();
+        }
+    }
+}
