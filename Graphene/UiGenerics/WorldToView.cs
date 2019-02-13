cd05aa3fd7701985aabd86d4f2d787aed435689a
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2019-02-13 19:33:11 -0200

    add world to view

---
+++ Graphene/UiGenerics/WorldToView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Graphene.UiGenerics
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class WorldToView : MonoBehaviour
+    {
+        private RectTransform _rect;
+
+        public Transform Target;
+        private Camera _cam;
+
+        private void Awake()
+        {
+            _rect = transform as RectTransform;
+            _cam = Camera.main;
+        }
+
+        private void Update()
+        {
+            if(Target == null) return;
+
+            var pos = _cam.WorldToScreenPoint(Target.position);
+
+            _rect.position = pos;
+        }
+    }
+}
